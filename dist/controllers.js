1eda86525aca6a99644f88570bb0ca56431e11c1
Guilherme de Araujo Sousa <guilherme.sousa@funchal.corp>
2018-06-12 21:14:49 -0300

    angular 1.6 $get

--- dist/controllers.js
+++ dist/controllers.js
@@ -4,9 +4,21 @@ angular.module('starter.controllers', [])
 var app =  angular.module("lilreddit", []);
 app.controller("lilredditCtrl", function($scope, $http){
 
-  $http.get('https://reddit.com/.json').success(function(rlist) {
-    $scope.rlist = rlist.data.children;
-  });
+  $http({
+    method: 'GET',
+    url: 'https://reddit.com/.json'
+  }).then(function successCallback(response) {
+    var obj = [];
+
+    angular.forEach(response.data.data.children, function(value, key) {
+      obj.push(value.data);
+    });
+    
+    $scope.rlist = obj;
+
+    }, function errorCallback(response) {
+      $scope.rlist = {"error":"=("};
+    });
 
 
 });
